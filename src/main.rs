32cbafa96c1cb8dd87bfcb5742acfd8c296644c7
Control Owl <control-owl@r-o0-t.wtf>
2025-09-21 19:37:39 +0200

    basic elements

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use egui::{ComboBox, Frame};
 use egui_extras::{TableBuilder, Column};
 use std::collections::VecDeque;
 
+const GUI_MARGINS: usize = 20;
+
 #[derive(Debug)]
 struct AddressTable {
   index: u32,
@@ -36,6 +38,7 @@ impl CryptoWallet {
       private_key: "5J1F...".into(),
     });
 
+    // TODO: Get values from local config
     Self {
       address_data,
       entropy_source: "RNG".to_string(),
@@ -44,60 +47,152 @@ impl CryptoWallet {
     }
   }
 
-  fn render_table(&mut self, ui: &mut egui::Ui) {
-    TableBuilder::new(ui)
-      .striped(true)
-      .resizable(true)
-      .column(Column::auto()) // Index
-      .column(Column::remainder().at_least(100.0)) // Coin
-      .column(Column::remainder().at_least(100.0)) // Path
-      .column(Column::remainder().at_least(120.0)) // Address
-      .column(Column::remainder().at_least(120.0)) // Public Key
-      .column(Column::remainder().at_least(120.0)) // Private Key
-      .header(22.0, |mut header| {
-        for title in ["Index", "Coin Name", "Path", "Address", "Public Key", "Private Key"] {
-          header.col(|ui| { ui.label(title); });
-        }
-      })
-      .body(|body| {
-        body.rows(20.0, self.address_data.len(), |mut row| {
-          let address_row = &self.address_data[row.index()];
+  fn render_wallet_header(&mut self, ui: &mut egui::Ui) {
+    ui.vertical_centered(|ui| {
+      ui.heading("Your crypto, your entropy, your control");
+    });
 
-          row.col(|ui| { ui.label(address_row.index.to_string()); });
-          row.col(|ui| { ui.label(&address_row.coin); });
-          row.col(|ui| { ui.label(&address_row.path); });
-          row.col(|ui| { ui.label(&address_row.address); });
-          row.col(|ui| { ui.label(&address_row.public_key); });
-          row.col(|ui| { ui.label(&address_row.private_key); });
+    ui.add_space(GUI_MARGINS as f32);
+
+    ui.horizontal_top(|ui| {
+      ui.horizontal(|ui| {
+        Frame::group(ui.style()).show(ui, |ui| {
+          ui.vertical(|ui| {
+            ComboBox::from_label("Entropy Source")
+              .selected_text(&self.entropy_source)
+              .show_ui(ui, |ui| {
+                ui.selectable_value(&mut self.entropy_source, "RNG".to_string(), "RNG");
+                ui.selectable_value(&mut self.entropy_source, "QRNG".to_string(), "QRNG");
+                ui.selectable_value(&mut self.entropy_source, "File".to_string(), "File");
+              });
+
+            ui.add_space(GUI_MARGINS as f32 / 2.0);
+
+            ui.vertical(|ui| {
+              ui.horizontal_wrapped(|ui| {
+                ui.spacing_mut().item_spacing.x = 0.0;
+                ui.code("RNG:");
+                ui.label(" Uses your device’s built-in random number generator. Fast and easy, but slightly less unpredictable");
+              });
+
+              ui.horizontal_wrapped(|ui| {
+                ui.spacing_mut().item_spacing.x = 0.0;
+                ui.code("QRNG:");
+                ui.label(" Uses quantum processes to create highly unpredictable numbers. Strongest randomness for security.");
+              });
+
+              ui.horizontal_wrapped(|ui| {
+                ui.spacing_mut().item_spacing.x = 0.0;
+                ui.code("File:");
+                ui.label(" Uses the contents of a file you provide as a source of randomness. Security depends on the file’s uniqueness.");
+              });
+            });
+          });
         });
       });
+
+      ui.add_space(GUI_MARGINS as f32);
+
+      ui.horizontal(|ui| {
+        Frame::group(ui.style()).show(ui, |ui| {
+          ui.vertical(|ui| {
+            ComboBox::from_label("Derivation Path")
+              .selected_text(self.derivation_path.to_string())
+              .show_ui(ui, |ui| {
+                ui.selectable_value(&mut self.derivation_path, 32, "32");
+                ui.selectable_value(&mut self.derivation_path, 44, "44");
+              });
+
+            ui.add_space(GUI_MARGINS as f32 / 2.0);
+
+            ui.vertical(|ui| {
+              ui.horizontal_wrapped(|ui| {
+                ui.spacing_mut().item_spacing.x = 0.0;
+                ui.code("32:");
+                ui.label(" Classic hierarchical wallet derivation.");
+              });
+
+              ui.horizontal_wrapped(|ui| {
+                ui.spacing_mut().item_spacing.x = 0.0;
+                ui.code("44:");
+                ui.label(" Structured derivation path used for multi-coin wallets.");
+              });
+            });
+          });
+        });
+      });
+    });
   }
 
-  fn render_controls(&mut self, ui: &mut egui::Ui) {
-    ui.horizontal_centered(|ui| {
-      if self.address_data.len() < self.max_rows {
-        if ui.button("Generate wallet").clicked() {
-          let next_index = self.address_data.back().map_or(0, |r| r.index + 1);
+  fn render_wallet_table(&mut self, ui: &mut egui::Ui) {
+    let available_height = ui.available_height();
+    Frame::new()
+      // .outer_margin(egui::Margin::same(GUI_MARGINS as i8 / 4))
+      .show(ui, |ui| {
+        TableBuilder::new(ui)
+        .striped(true)
+        .resizable(true)
+        .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+        .min_scrolled_height(0.0)
+        .max_scroll_height(available_height)
+        .animate_scrolling(false)
+        .column(Column::auto()) // Index
+        .column(Column::remainder().at_least(100.0)) // Coin
+        .column(Column::remainder().at_least(100.0)) // Path
+        .column(Column::remainder().at_least(120.0)) // Address
+        .column(Column::remainder().at_least(120.0)) // Public Key
+        .column(Column::remainder().at_least(120.0)) // Private Key
+        .header(GUI_MARGINS as f32, |mut header| {
+          for title in ["Index", "Coin Name", "Path", "Address", "Public Key", "Private Key"] {
+            header.col(|ui| { ui.label(title); });
+          }
+        })
+        .body(|body| {
+          body.rows(GUI_MARGINS as f32, self.address_data.len(), |mut row| {
+            let address_row = &self.address_data[row.index()];
 
-          // TODO: Generate new wallet
-
-          // Sample data
-          self.address_data.push_back(AddressTable {
-            index: next_index,
-            coin: "ETHEREUM CLASSIC LONG TEXT".into(),
-            path: "m/44'/61'/0'/0/0'".into(),
-            address: "0xdFe31394A33c9C1c7D9FC9b33E90fdc3a0D7FBd1".into(),
-            public_key: "0x0212a96b15c77f95473d4c6d2c0efe5eb287684be1a6a0243cff1c7d6571e8c3fb".into(),
-            private_key: "0x85f7ac69dc2bbf45d6145823ec161f7177ec83ce7fd112e3fa38015b89d".into(),
+            row.col(|ui| { ui.label(address_row.index.to_string()); });
+            row.col(|ui| { ui.label(&address_row.coin); });
+            row.col(|ui| { ui.label(&address_row.path); });
+            row.col(|ui| { ui.label(&address_row.address); });
+            row.col(|ui| { ui.label(&address_row.public_key); });
+            row.col(|ui| { ui.label(&address_row.private_key); });
           });
-        }
-      } else {
-        ui.label("Memory limit reached—cannot generate more wallets.");
-      }
+        });
+    });
+  }
 
-      if ui.button("Delete wallet").clicked() {
-        self.address_data.clear();
-      }
+  fn render_wallet_footer(&mut self, ui: &mut egui::Ui) {
+    Frame::new()
+      .outer_margin(egui::Margin::same(GUI_MARGINS as i8))
+      .show(ui, |ui| {
+        ui.horizontal_centered(|ui| {
+          if self.address_data.len() < self.max_rows {
+            if ui.button("Generate wallet").clicked() {
+              let next_index = self.address_data.back().map_or(0, |r| r.index + 1);
+
+              // TODO: Generate new wallet
+
+              // Sample data
+              self.address_data.push_back(AddressTable {
+                index: next_index,
+                coin: "ETHEREUM CLASSIC".into(),
+                path: "m/44'/61'/0'/0/0'".into(),
+                address: "0xdFe31394A33c9C1c7D9FC9b33E90fdc3a0D7FBd1".into(),
+                public_key: "0x0212a96b15c77f95473d4c6d2c0efe5eb287684be1a6a0243cff1c7d6571e8c3fb".into(),
+                private_key: "0x85f7ac69dc2bbf45d6145823ec161f7177ec83ce7fd112e3fa38015b89d".into(),
+              });
+            }
+            ui.add_space(GUI_MARGINS as f32);
+          } else {
+            ui.label("Memory limit reached—cannot generate more addresses.");
+            ui.add_space(GUI_MARGINS as f32);
+          }
+
+          if ui.button("Delete wallet").clicked() {
+            self.address_data.clear();
+          }
+      });
     });
   }
 }
@@ -105,47 +200,31 @@ impl CryptoWallet {
 impl eframe::App for CryptoWallet {
   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
     egui::TopBottomPanel::top("header").show(ctx, |ui| {
-      ui.horizontal_centered(|ui| {
-        Frame::group(ui.style()).show(ui, |ui| {
-          ComboBox::from_label("Entropy Source")
-            .selected_text(&self.entropy_source)
-            .show_ui(ui, |ui| {
-              ui.selectable_value(&mut self.entropy_source, "RNG".to_string(), "RNG");
-              ui.selectable_value(&mut self.entropy_source, "QRNG".to_string(), "QRNG");
-              ui.selectable_value(&mut self.entropy_source, "File".to_string(), "File");
-            });
-        });
-
-        Frame::group(ui.style()).show(ui, |ui| {
-          ComboBox::from_label("Derivation Path")
-            .selected_text(self.derivation_path.to_string())
-            .show_ui(ui, |ui| {
-              ui.selectable_value(&mut self.derivation_path, 32, "32");
-              ui.selectable_value(&mut self.derivation_path, 44, "44");
-            });
-        });
-      });
+      ui.add_space(GUI_MARGINS as f32);
+      self.render_wallet_header(ui);
+      ui.add_space(GUI_MARGINS as f32 / 2.0);
     });
+
 
     egui::CentralPanel::default().show(ctx, |ui| {
       egui::ScrollArea::both().show(ui, |ui| {
-        self.render_table(ui);
+        self.render_wallet_table(ui);
       });
     });
 
     egui::TopBottomPanel::bottom("footer").show(ctx, |ui| {
-        self.render_controls(ui);
+        self.render_wallet_footer(ui);
     });
 
-    // Avoid redrawing too often when inserting rows
-    ctx.request_repaint_after(std::time::Duration::from_millis(100));
+    // ctx.request_repaint_after(std::time::Duration::from_millis(100));
   }
 }
 
 fn main() -> Result<(), eframe::Error> {
   let options = eframe::NativeOptions {
     viewport: egui::ViewportBuilder::default()
-      .with_inner_size([800.0, 600.0]),
+      .with_inner_size([800.0, 600.0])
+      .with_min_inner_size([200.0, 300.0]),
     ..Default::default()
   };
 
